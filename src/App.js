6808ca583ea000fb82b1695b3d88fc5ad33cadfb
Ross Mawdsley <rossmawd@gmail.com>
2019-12-03 22:46:10 +0000

    using Analytics.record

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react';
+import { Analytics, AnalyticsClass } from 'aws-amplify'
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+class App extends React.Component {
+
+  recordEvent = () => {
+    console.log("about to record event")
+    Analytics.record({
+      name: 'Test Event 1',
+      attributes: {
+        username: 'dabit3'
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">``
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to HELLo.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <button onClick={this.recordEvent} >Record Event</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App);
